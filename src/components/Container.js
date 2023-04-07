6c57ac31dd2b21429b04a28ec6998f2f757120f3
daklo91 <coffeedog91@gmail.com>
2023-04-07 18:55:51 +0200

    Update container

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,8 +3,8 @@ import styles from "./Container.module.css";
 function Container() {
   return (
     <div className={styles.container}>
-      <div className={styles.column1}></div>
-      <div className={styles.column2}></div>
+      <div className={styles.column}>Dette er en test</div>
+      <div className={styles.column}>Dette er en test</div>
     </div>
   );
 }
