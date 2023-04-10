8d73836530576c745c428510c57a78782edf2dcc
daklo91 <coffeedog91@gmail.com>
2023-04-10 14:28:14 +0200

    Add counter

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,12 +1,32 @@
 import styles from "./Container.module.css";
+import { useEffect, useState } from "react";
 
 function Container() {
+  const [scoreNumber] = useState(50);
+  const [tickNumber, setTickNumber] = useState(0);
+  const [delay, setDelay] = useState(10);
+
+  useEffect(() => {
+    const tickTreshold = scoreNumber - 30;
+    const ticker = setInterval(() => {
+      if (tickNumber > tickTreshold && tickNumber !== scoreNumber) {
+        setDelay((prev) => prev + 6);
+      }
+      if (tickNumber < scoreNumber) {
+        setTickNumber((prev) => prev + 1);
+      } else {
+        clearInterval(ticker);
+      }
+    }, delay);
+    return () => clearInterval(ticker);
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.showcase}>
         <p className={`${styles.yourResult} heading-m`}>Your Result</p>
         <div className={styles.counterContainer}>
-          <p className={`${styles.counterScore} heading-xl`}>76</p>
+          <p className={`${styles.counterScore} heading-xl`}>{tickNumber}</p>
           <p className={styles.counterTotalScore}>of 100</p>
         </div>
         <p className={`${styles.resultInfoHeader} heading-l`}>Great</p>
